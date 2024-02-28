f5d87479866cecc8b0c350660f77be353bff1163
ambrosia13 <ivynow.13@gmail.com>
2024-02-28 14:07:16 -0800

    Optimize uv transformation

--- src/util/mesh.rs
+++ src/util/mesh.rs
@@ -46,24 +46,15 @@ impl ChunkMeshBuilder {
     ) {
         let one_texel = (1.0 / atlas_size.0 as f32, 1.0 / atlas_size.1 as f32);
 
-        let starting_x = atlas_coords.min.0;
-        let starting_y = atlas_coords.min.1;
+        let starting_x = atlas_coords.min.0 as f32 * one_texel.0;
+        let starting_y = atlas_coords.min.1 as f32 * one_texel.1;
 
-        let ending_x = atlas_coords.max.0;
-        let ending_y = atlas_coords.max.1;
+        let ending_x = atlas_coords.max.0 as f32 * one_texel.0;
+        let ending_y = atlas_coords.max.1 as f32 * one_texel.1;
 
         for uv in uvs.iter_mut() {
-            if uv[0] < 0.5 {
-                uv[0] = starting_x as f32 * one_texel.0;
-            } else {
-                uv[0] = ending_x as f32 * one_texel.0;
-            }
-
-            if uv[1] < 0.5 {
-                uv[1] = starting_y as f32 * one_texel.1;
-            } else {
-                uv[1] = ending_y as f32 * one_texel.1;
-            }
+            uv[0] = uv[0] * (ending_x - starting_x) + starting_x;
+            uv[1] = uv[1] * (ending_y - starting_y) + starting_y;
         }
     }
 
